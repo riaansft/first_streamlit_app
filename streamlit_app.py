30f96a65d611163961eadc6507fe89189539e24b
Riaan <116603326+riaansft@users.noreply.github.com>
2022-10-24 16:04:57 -0700

    Update streamlit_app.py
    Added lines for menu name and menu items.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit as sl
 
 sl.title('My Parents New Healthy Diner.')
+
+sl.header('Breakfast Menu')
+
+sl.text('Omega 3 & Blueberry Oatmeal')
+sl.text('Kail, Spinach & Rocket Smoothy')
+sl.text('Hard-Boiled Free-Range Egg')
