8075e78348dfc5c3ca77c4b6234a9757a9e0b290
Riaan <116603326+riaansft@users.noreply.github.com>
2022-10-24 16:30:18 -0700

    Update streamlit_app.py
    Added ability to sore selected items and only show selected items in list to user

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,12 @@ sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index("Fruit")
 
-# Add picklist so users can choose fruit from list and set examples in [ ]
-sl.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
+# Add picklist so users can choose fruit from list and set examples in [ ]. Store fruits selected by user in variable.
+fruits_selected = sl.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-# Display list in table
-sl.dataframe(my_fruit_list)
+# Display full list in table
+# sl.dataframe(my_fruit_list)
+
+# Display selected items in table
+sl.dataframe(fruits_to_show)
