9cedf4eacfa02899e86a24773eee04e47259d262
Riaan <116603326+riaansft@users.noreply.github.com>
2022-10-24 16:10:21 -0700

    Update streamlit_app.py
    Added Avo Toast, Emojis and 2nd menu header.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,15 @@
 import streamlit as sl
 
+# Add Main Title
 sl.title('My Parents New Healthy Diner.')
 
-sl.header('Breakfast Menu')
+# Add Menu 1
+sl.header('Breakfast Favorites')
 
-sl.text('Omega 3 & Blueberry Oatmeal')
-sl.text('Kail, Spinach & Rocket Smoothy')
-sl.text('Hard-Boiled Free-Range Egg')
+sl.text('🥣 Omega 3 & Blueberry Oatmeal')
+sl.text('🥗 Kail, Spinach & Rocket Smoothy')
+sl.text('🐔 Hard-Boiled Free-Range Egg')
+sl.text('🥑🍞 Avocado Toast')
+
+# Add Menu 2
+sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
