b22086ff60466181aecb246bc5803062dd1af77e
Riaan <116603326+riaansft@users.noreply.github.com>
2022-11-06 19:00:50 -0800

    Update streamlit_app.py
    remove 'from sl' line

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,7 +67,7 @@ if sl.button('Get fruit load list'):
 # Add another input box to add fruit!
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
-    my_cur.execute("INSERT INTO fruit_load_list VALUES ('from sl')")
+    my_cur.execute("INSERT INTO fruit_load_list VALUES ('" + new_fruit + "')")
     return "Thanks for adding " + new_fruit
     
 add_my_fruit = sl.text_input('What fruit would you like to add?')
